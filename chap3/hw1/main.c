b444d3d9cacee9557527354cf8c90774b8e3b0e5
LimYonggi <dydrlfkd2@gmail.com>
2024-09-28 11:48:32 +0000

    Assignment completed

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -3,29 +3,45 @@
 #include "copy.h"
 
 char line[MAXLINE];
-char longest[MAXLINE];
+char longest[5][MAXLINE];
+char temp[MAXLINE];
 
 int main() {
 	int len;
 	int max;
-	int i;
+	int i = 0;
+	int l[5];
+	int count = 0;
 	max = 0;
-	i = 0;
 
-	while(gets(line) != NULL && i <= 4) {
+	while(i < 5 && gets(line) != NULL) {
 	len = strlen(line);
-
+	l[i] = len;
+	copy(line, longest[i]);
 		if(len > max) {
 			max = len;
-			copy(line, longest);
 		}
 		i++;
+		count++;
+	} 
+	for(int j = 0; j < count - 1; j++){
+		for(int k = j + 1; k < count; k++) {
+			if(l[j] < l[k]){
+		 		int Len = l[j];
+				l[j] = l[k];
+				l[k] = Len;
+
+				copy(longest[j], temp);
+				copy(longest[k], longest[j]);
+				copy(temp, longest[k]);
+				}
+		}
 	}
-    
+
+
 	if(max > 0){
-		while(i <= 0){
-		printf("%s \n", longest);
-		i--;
+		for(int j= 0; j < count; j++){
+		printf("%s \n", longest[j]);
 		}
 	}
 	return 0;
