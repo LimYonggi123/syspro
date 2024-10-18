7f23e0ec453f592c4a29f4e172a52fbc89d47bc7
LimYonggi <dydrlfkd2@gmail.com>
2024-10-18 03:49:05 +0000

    chap4 complete

--- chap4/hw2/cat.c
+++ chap4/hw2/cat.c
@@ -4,22 +4,23 @@ int main(int argc, char *argv[])
 {
 	char c;
 	FILE  *fp1, *fp2;
-	if(argc != 3) {
+	if(argc < 2) {
 		fprintf(stderr, "How to use:%s File1 File2\n", argv[0]);
 		return 1;
 	}
-   fp1 = fopen(argv[1], "r");
+	for(int i = 1; i <argc; i++) { 
+   fp1 = fopen(argv[i], "r");
    if(fp1 == NULL) {
-	   fprintf(stderr, "File %s Open Error\n", argv[1]);
+	   fprintf(stderr, "File %s Open Error\n", argv[i]);
 	   return 2;
    }
 
-   fp2 = fopen(argv[2], "a");
    while((c = fgetc(fp1)) != EOF)
-	   fputc(c, fp2);
+	   putchar(c);
     
    fclose(fp1);
-   fclose(fp2);
+	}
+
    return 0;
 }
 
